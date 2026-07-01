668129a32022894f7b7fe2837fb29297514b3970
Stefanus Yudi Irwan <stefanus.yudi96@gmail.com>
2026-07-02 06:52:21 +0700

    feat: learn about module and standard javascript

---
+++ node-js/node-js-basic/standard-library.js
@@ -0,0 +1,7 @@
+import os from "os";
+
+console.info(os.platform());
+console.table(os.cpus());
+
+// tidak bisa import karena extention filenya .js
+// bisa import tapi menggunakan require karena extention filenya .js
